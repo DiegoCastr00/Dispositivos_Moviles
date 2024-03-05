a04f0c500ed86f4aa198550f776cf83721cb35c8
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-03-04 23:42:08 -0600

    primera parte de 3

---
+++ PRAC_3_Acelerometro/app/src/main/java/com/example/prac_3_acelerometro/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.prac_3_acelerometro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometerSensor: Sensor
+    private lateinit var sensorEventListener: SensorEventListener
+    private lateinit var tvInclinacion: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvInclinacion = findViewById(R.id.tvInclinacion)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!!
+
+        sensorEventListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+                    val x = event.values[0]
+                    val y = event.values[1]
+                    val z = event.values[2]
+                    if (y > 9.0) {
+                        tvInclinacion.text = "¡Inclinacion hacia arriba!"
+                        tvInclinacion.text = "¡Inclinacion hacia arriba!"
+                    }
+                    if (x > 9.0) {
+                        tvInclinacion.text = "¡Inclinacion hacia un lado!"
+                    }
+                    if (z > 9.0) {
+                        tvInclinacion.text = "Posicion normal"
+                    }
+                }
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+            }
+        }
+    }
+    override fun onResume(){
+        super.onResume()
+        sensorManager.registerListener(
+            sensorEventListener,
+            accelerometerSensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(sensorEventListener)
+    }
+}
